fcf71d42d62bbea9d5b3218ce750c62bb2d11bed
Nate Dow <ndow33@gmail.com>
2020-10-02 09:43:31 -0600

    update

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,23 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # create a dictionary
+    d = {}
+    # loop through the list
+    for value in a:        
+        # check to see if the value is positive
+        if value > 0:
+            # if the value is not in the dictionary
+            if value not in d:
+                # add it to the dictionary
+                d[value] = 1  
+    # create an empty list to store the answers
+    result = []
+    # loop through the inverse of the list
+    a = [-1*(value) for value in a]
+    for value in a:
+        if value in d:
+            result.append(value)
 
     return result
 
