a2da61bd656c621103c6b96ee6841dd0f45516c1
Nate Dow <ndow33@gmail.com>
2020-10-02 11:28:04 -0600

    update

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    d = dict()
+    if length >= 2:
+        # store each index in the dictionary by its weight
+        for i in range(0,len(weights)):
+            d[weights[i]] = i
+    
+        # loop through the weights again
+        for i in range(0,len(weights)):
+            # find the difference between the weight and the limit
+            dif = limit-weights[i]
+            # if the dif is in our dictionary
+            if dif in d:
+                # return the indices
+                return (d[dif], i)
     return None
